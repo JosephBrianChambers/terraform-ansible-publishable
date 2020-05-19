3ec3b2285f6289267cdc9ae0f98624e0d521edc6
Joe Chambers <josephbrianchambers@gmail.com>
2020-05-19 11:47:12 -0700

    Assign key pair, Add allow_ssh security group, enable ssh

--- provision_instance_aws.tf
+++ provision_instance_aws.tf
@@ -9,9 +9,36 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami = "ami-0f56279347d2fa43e"
   instance_type = "t3.nano"
+  key_name = "aws_play"
+  security_groups = ["allow_ssh"]
 
   tags = {
     Name = "web"
     project = "terraform"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  # vpc_id: using default AWS account VPC
+
+  ingress {
+    description = "SSH from world"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # tags = {}
+}
+
+
